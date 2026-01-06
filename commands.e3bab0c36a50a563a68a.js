c4b82d47898fed88281626d1e4851d4c0f065517
RobertLeRobot <robert.lerobot@actuelia.fr>
2026-01-06 19:25:16 +0100

    restauration

---
+++ commands.e3bab0c36a50a563a68a.js
@@ -0,0 +1,19 @@
+(() => {
+    "use strict";
+    const e = "ms-excel:ofe|u|https://actuelia.sharepoint.com/:x:/r/sites/Planning/_layouts/15/Doc.aspx?sourcedoc=%7BB2D3EAFD-27C3-4815-A36C-791752E43822%7D&file=Planning%20-%20v11.xlsx&action=default&mobileredirect=true";
+    async function n(n) {
+        try {
+            await Excel.run(async n => {
+                const o = n.workbook.properties;
+                o.load("title"), await n.sync(), "PlanningActuelia" === o.title ? (console.log("Déjà sur le bon fichier, affichage du taskpane."), await Office.addin.showAsTaskpane()) : (console.log("Autre fichier, ouverture du planning SharePoint."), window.open(e, "_blank"))
+            })
+        } catch (n) {
+            console.error("Erreur dans openPlanningFile:", n), window.open(e, "_blank")
+        } finally {
+            n && n.completed && n.completed()
+        }
+    }
+    Office.onReady(() => {
+        console.log("Complément Planning Actuelia prêt."), n()
+    }), Office.actions.associate("openPlanningFile", n)
+})();
